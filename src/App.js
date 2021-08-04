1db5b6988c03594f01e5691057392d5f42ffcc73
unknown <999barbell@gmail.com>
2021-08-04 20:53:37 +0300

    hw2-feedback

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+import SectionTitle from './components/SectionTitle';
+import FeedbackOptions from './components/FeedbackOptions';
+import Statistics from './components/Statistics';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = evt => {
+    const key = evt.target.textContent.toLowerCase();
+
+    this.setState(prevState => ({
+      [key]: prevState[key] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, value) => (acc += value), 0);
+  };
+
+  countPositiveFeedbackPercentage = total => {
+    const positivePercentage = (this.state.good * 100) / total;
+    return positivePercentage;
+  };
+
+  render() {
+    const total = this.countTotalFeedback();
+    const positivePercentage = Math.round(
+      this.countPositiveFeedbackPercentage(total),
+    );
+
+    return (
+      <SectionTitle>
+        <FeedbackOptions
+          options={this.state}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+        <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+          total={total}
+          positivePercentage={positivePercentage}
+        />
+      </SectionTitle>
+    );
+  }
+}
+
+export default App;
